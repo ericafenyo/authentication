db8823ca59e01104958432715db0a9b4c9224eea
eric <eric.afenyo@transway.fr>
2021-02-09 00:48:52 +0100

    create a company

---
+++ src/company/company.resolver.ts
@@ -0,0 +1,65 @@
+import {
+  Resolver,
+  InputType,
+  Field,
+  Mutation,
+  Query,
+  Args,
+  ObjectType,
+  ID,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
+import { UserType } from 'src/user/user.resolver';
+import { Company } from './company.schema';
+import { CompanyService } from './company.service';
+
+@InputType()
+export class CompanyInput {
+  @Field()
+  name: string;
+
+  @Field()
+  firstName: string;
+
+  @Field()
+  lastName: string;
+
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+export class CompanyType {
+  @Field()
+  name: string;
+
+  @Field(() => ID, { name: 'id' })
+  _id: string;
+
+  @Field()
+  user: UserType;
+}
+
+@Resolver(() => CompanyType)
+export class CompanyResolver {
+  constructor(private readonly companyService: CompanyService) {}
+
+  @Query(() => CompanyType, { name: 'company' })
+  async getCompany(@Args('companyId') companyId: string) {
+    return await this.companyService.findOne(companyId);
+  }
+
+  @Mutation(() => CompanyType, { name: 'company' })
+  async createCompany(@Args('input') input: CompanyInput): Promise<Company> {
+    return await this.companyService.create(input);
+  }
+
+  @ResolveField()
+  async user(@Parent() company: Company) {
+    return await this.companyService.getUser(company.userId);
+  }
+}
---
+++ src/company/company.schema.ts
@@ -0,0 +1,17 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { User } from 'src/user/user.schema';
+
+@Schema()
+export class Company extends Document {
+  @Prop()
+  name: string;
+
+  @Prop({ type: Types.ObjectId, ref: User })
+  userId: string;
+
+  @Prop({ type: [Types.ObjectId] })
+  tenants: string[];
+}
+
+export const CompanySchema = SchemaFactory.createForClass(Company);
